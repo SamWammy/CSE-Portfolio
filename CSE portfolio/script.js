3cfae05d982ef99ee3e3797c00a02abd64a60e4d
Sammy <143444689+SamWammy@users.noreply.github.com>
2025-03-03 10:26:59 -0500

    Add files via upload

---
+++ CSE portfolio/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    VANTA.BIRDS({
+      el: "#hero", 
+      quantity: 5,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.5,
+      scaleMobile: 1.00,
+      birdSize: 1.10,
+      wingSpan: 29.00,
+      backgroundAlpha: 0 
+    });
+  });
+  
